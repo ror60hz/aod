3f68f772a28aba1a1afb3df759d116bbbfd481eb
ror60hz <166310147+ror60hz@users.noreply.github.com>
2025-09-08 07:49:58 +0900

    Create Hook.java

---
+++ app/src/main/java/com/aodfix/Hook.java
@@ -0,0 +1,25 @@
+package com.aodfix;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+public class Hook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.transsion.aod")) {
+            try {
+                findAndHookMethod(
+                    "com.transsion.aod.AODManager",
+                    lpparam.classLoader,
+                    "getTimeout",
+                    new XC_MethodReplacement() {
+                        @Override
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                            return 60000; // 1 menit
+                        }
+                    }
+                );
+            } catch (Throwable t) { }
+        }
+    }
+}
